83196b987d9c57c6a0a9c43f89c0d0c67cfe2093
soorajsharma712-byte <sooraj.sharma.712@gmail.com>
2025-11-09 12:52:05 +0530

    pycharm
    this is my assignment for tutedude task 1 and task 2

---
+++ task2.py
@@ -0,0 +1,10 @@
+
+name = input("Enter your name: "). strip()
+surname = input("Enter your surname: "). strip()
+full_name =  name + " " + surname
+print("hello", full_name ,"welcome to my assignment")
+
+
+
+
+
