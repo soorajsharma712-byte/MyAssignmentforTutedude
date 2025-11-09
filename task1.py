03d3c8083ff4b2a05ad7f12fe1249eb2c1233ecd
soorajsharma712-byte <sooraj.sharma.712@gmail.com>
2025-11-09 13:37:24 +0530

    suraj kumar
    this is assignment for tutedude task1 basic arithematic concept

---
+++ task1.py
@@ -0,0 +1,21 @@
+
+# Author: SURAJ KUMAR
+# Assignment: Basic Arithematic Concept - Task1
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (cannot diveded by zero)"
+
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
+
