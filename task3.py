3eef767c94f1d7a2185876b3a736ea4c56287f68
soorajsharma712-byte <sooraj.sharma.712@gmail.com>
2025-11-26 21:51:39 +0530

    Add files via upload
    This is my task 3 of my TuteDude assignment : Write a Python program that take an integer input from the user and check whether the number is even or odd using an if-else statement.

---
+++ task3.py
@@ -0,0 +1,8 @@
+#Program to check the is Even or Odd
+#Author SURAJ KUMAR
+
+num: int = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num,"is an even number. ")
+else:
+    print(num,"is an odd number. ")
